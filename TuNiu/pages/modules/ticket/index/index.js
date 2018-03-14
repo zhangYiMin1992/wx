9023adf53368f7531f89f055ad7881c03f646f7b
simple <470779536@qq.com>
2018-03-14 18:09:27 +0800

    首页列表渲染完成

--- TuNiu/pages/modules/ticket/index/index.js
+++ TuNiu/pages/modules/ticket/index/index.js
@@ -40,21 +40,184 @@ Page({
     cache.store.put('index',{isrender:1});
   },
   onShow: function () {
-  
+    let index=cache.store.get('index');
+    if(index && index.isrender == 0){
+      if(index.tabIndex == 2 && index.isrender_ol == 1){
+        this.setData({
+          tabIndex:2,
+          orderList:[],
+          pageNum:1,
+          pageEnd:false
+        });
+        this.getOrderList();
+      }
+      return;
+    }
+    //页面显示
+    let city=cache.store.get(SELECTED_CITY);
+    let name=city?city.name : this.data.keyword;
+    let code=city?city.code : this.data.cityCode;
+    let des= cache.store.get(SELECTED_DES);
+    this.setData({
+      keyword : des ? des : name,
+      cityCode:code,
+      header:{
+        name:'index',
+        city:cityLthLimit(name),
+        des:des
+      },
+      page:1,
+      location:[],
+      list:[],
+      loadText:"正在加载中..."
+    });
+    this.getPageData();
+    this.getCurrentPosition();
   },
-  onHide: function () {
-  
+  onShareAppMessage(){
+    let city = cache.store.get("TICKET_SELECTED_CITY") || {name:'北京'};
+    let cityName = city.name;
+    return{
+      title:'景点门票',
+      desc:'一起去玩吧！',
+      path: '/pages/modules/ticket/index/index?sharename=' + encodeURIComponent(cityname)
+    }
   },
-  onUnload: function () {
-  
+  modifyList(list) {
+    for(let i = 0, len = list.length; i <len; i++) {
+      list[i].image = httpsProtocol(list[i].image);
+    }
+    return list;
   },
-  onPullDownRefresh: function () {
-  
+  bindShowCity:function(){
+    cache.store.put('index',{isrender:0});
+    ge2Page({url:'../city-picker/city-picker'});
   },
-  onReachBottom: function () {
-  
+  getPageData:function(){
+    let self = this;
+    let data = self.data;
+    wx.request({
+      url:APIs.search,
+      data:{
+        d:{"page":data.page, "limit":PAGE_SIZE,"keyword":self.data.keyword,"location":[]},
+        c:{"cc":self.data.citycode}
+      },
+      header:{
+        'content-type':'application/json'
+      },
+      success:function(res){
+        let json = res.data;
+        let loadText = "正在加载中...";
+        if(json.success){
+          let nextPage=json.data.list;
+          nextPage=self.modifyList(nextPage);
+          if(nextPage && nextPage.length > 0){
+            let list = data.list;
+            list.push.apply(list,nextPage);
+            self.setData({
+              list:list
+            })
+            loadText = list.length < 10 ? "亲，已经加载完啦～" : "正在加载中...";
+          } else {
+            loadText = self.data.page == 1 ? "暂无数据" : "亲，已经加载完啦～";
+          }
+          self.setData({
+            loadText:loadText
+          });
+          wx.hideToast();
+        }
+      },
+      fail:function(){
+        let loadText = '您的网络断开，请重新连接';
+        cache.store.put('index', {isrender:1});
+        self.setData({
+          loadText: loadText
+        });
+        wx.hideToast();
+      }
+    })
   },
-  onShareAppMessage: function () {
-  
+  getCurrentPosition: function(){
+    let self = this;
+    let curTimestamp = +(new Date);
+    let lastTimestamp = cache.store.get(CITY_SWITCH_TS);
+    //用户选择间隔小于3小时，不提示
+    if(lastTimestamp && lastTimestamp -  curTimestamp < EXPIRED){
+      wx.showToast({
+        title:'加载中',
+        icon:'loading',
+        duration:10000,
+        mask:true
+      })
+      return;
+    }
+    //获取定位城市
+    cache.store.put('index',{isrender:0});
+    if(isReject) return;
+    wx.getLocation({
+      complete:function(res){
+        var latitude = res.latitude
+        var longitude = res.longitude
+        if(!(res && res.latitude && res.longitude) && !isReject){
+          isReject = true;
+          return;
+        }
+        var obj={
+          location:{
+            lat: latitude,
+            lng:longitude
+          }
+        }
+        wx.request({
+          url:APIs.getCityCode + '?data=' + JSON.stringify(obj),
+          method:'GET',
+          success:function(res){
+            let code = res.data.data.location.code;
+            let name = res.data.data.location.name;
+            cache.store.put(GEO_CITY, {"code":code,"name":name});
+            if(self.data.cityCode == code){
+              cache.store.put(CITY_SWITCH_TS, +(new Date));
+              return;
+            }
+            wx.showModal({
+              title: '当前定位城市是' + name,
+              content: '是否切换',
+              success: function(res){
+                if(res.confirm){
+                  self.setData({
+                    page:1,
+                    list:[],
+                    'header.city':cityLthLimit(name),
+                    'header.des':'',
+                    keyword:name,
+                    citycode:code
+                  });
+                  wx.showToast({
+                    title: '加载中',
+                    icon:'loading',
+                    duration:10000,
+                    mask:true
+                  })
+                  cache.store.push(SELECTED_CITY,{"code": code,"name": name});
+                  self.getPageDate();
+                }
+              },
+              complete:function(res){
+                cache.store.put(CITY_SWITCH_TS, +(new Date));
+              }
+            })
+          }
+        });
+        
+      }
+    });
+  },
+  bindLowerList:function(){
+    this.setData({
+      page:this.data.page+1
+    });
+    this.getPageData();
   }
+
+  
 })
